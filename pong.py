f68fed3fd3bbf6b56c33817f198ce306adcbaf00
JoaoPorto23 <rm87082@fiap.com.br>
2021-09-27 17:21:17 -0300

    Creating collision

--- pong.py
+++ pong.py
@@ -5,19 +5,22 @@ pygame.init()
 clock = pygame.time.Clock()
 
 #Set do tamanho da tela do Pong
-screen_width = 1280
-screen_height = 960
+screen_width = 1000
+screen_height = 700
 screen = pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption('Pong')
 
 #Retângulos do Pong
 ball = pygame.Rect(screen_width/2-15, screen_height/2 - 15, 30,30)
-player = pygame.Rect(screen_width - 20, screen_height/2 - 70,10/140)
+player = pygame.Rect(screen_width - 20, screen_height/2 - 70,10,140)
 opponent = pygame.Rect(10, screen_height/2 - 70, 10, 140)
 
 #Variáveis de cor
 bg_color = pygame.Color('grey12')
 light_grey = (200,200,200)
+
+ball_speed_x = 7
+ball_speed_y = 7
 
 #Loop para fechar o tela
 while True:
@@ -25,6 +28,17 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+
+    if ball.top <= 0 or ball.bottom >= screen_height:
+        ball_speed_y *= -1
+    if ball.left <= 0 or ball.right >= screen_width:
+        ball_speed_x *= -1
+
+    if ball.colliderect(player) or ball.colliderect(opponent):
+        ball_speed_x *= -1
     #Visual
     screen.fill(bg_color)
     pygame.draw.rect(screen,light_grey, player)
