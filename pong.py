7c6e1dc05ef7371f7fe556a6c44c2fb8878921ee
JoaoPorto23 <rm87082@fiap.com.br>
2021-09-27 19:21:04 -0300

    Functional Pong

--- pong.py
+++ pong.py
@@ -1,4 +1,4 @@
-import pygame, sys
+import pygame, sys, random
 
 from pygame.constants import AUDIO_ALLOW_ANY_CHANGE
 
@@ -10,7 +10,7 @@ def ball_animation():
     if ball.top <= 0 or ball.bottom >= screen_height:
         ball_speed_y *= -1
     if ball.left <= 0 or ball.right >= screen_width:
-        ball_speed_x *= -1
+        ball_restart()
 
     if ball.colliderect(player) or ball.colliderect(opponent):
         ball_speed_x *= -1
@@ -32,6 +32,12 @@ def opponent_ai():
     if opponent.bottom >=screen_height:
         opponent.bottom = screen_height
 
+def ball_restart():
+    global ball_speed_x, ball_speed_y
+    ball.center = (screen_width/2, screen_height/2)
+    ball_speed_y *= random.choice((1,-1))
+    ball_speed_x *= random.choice((1,-1))
+
 #Setup geral
 pygame.init()
 clock = pygame.time.Clock()
@@ -51,8 +57,8 @@ opponent = pygame.Rect(10, screen_height/2 - 70, 10, 140)
 bg_color = pygame.Color('grey12')
 light_grey = (200,200,200)
 
-ball_speed_x = 7
-ball_speed_y = 7
+ball_speed_x = 7 * random.choice((1,-1))
+ball_speed_y = 7 * random.choice((1,-1))
 player_speed = 0
 opponent_speed = 7
 
